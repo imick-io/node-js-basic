8e7f433f633e9fa6bc93117c17158819d12d9c18
Michael Boutin <michael@imick.io>
2024-03-04 10:06:28 -0500

    Return html file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 // --- CREATE SERVER ---
 // This request will run for every request that comes in for the server
@@ -9,4 +10,37 @@ http.createServer(rqListener);
 http.createServer(function (req, res) {});
 
 // OR (arrow function)
-http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+
+  // Home page
+  if (url === "/") {
+    //   Sending HTML Code
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && req.method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+
+  // CTRL + C to stop the server
+  //   process.exit(); // This will stop the server
+});
+
+server.listen(3000);
