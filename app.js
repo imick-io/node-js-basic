b21c5b9d4bcab22641c722e64dc3906e3a629df5
Michael Boutin <michael@imick.io>
2024-03-05 09:35:40 -0500

    Refactor into a routes file

--- app.js
+++ app.js
@@ -1,46 +1,31 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require("./routes");
+
+// NOTE: when running the server, it will be cached and so it will not be reloaded when updating the code
 
 // --- CREATE SERVER ---
 // This request will run for every request that comes in for the server
-function rqListener(req, res) {}
-http.createServer(rqListener);
+// function rqListener(req, res) {}
+// http.createServer(rqListener);
 
 // OR (anonymous function)
-http.createServer(function (req, res) {});
+// http.createServer(function (req, res) {});
 
 // OR (arrow function)
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-
-  // Home page
-  if (url === "/") {
-    //   Sending HTML Code
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-
-  if (url === "/message" && req.method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  res.write("</html>");
-
-  // CTRL + C to stop the server
-  //   process.exit(); // This will stop the server
-});
-
+const server = http.createServer(routes);
 server.listen(3000);
+
+// --- EVENT LOOP ---
+// Node.js is single threaded
+// But if Node.js is a single thread, how can it handle multiple requests at the same time?
+// Node will send the longer operations to the background (Worker Pool) to do the eavy lifting and continue to execute the code
+
+// So, if it is single threaded, is there a second thread? Like accessing the request and response from another request?
+
+// Event loop iterations:
+// - Timers (setInterval, setTimeout)
+// - Pending Callbacks (execute I/O callbacks deferred to the next loop iteration)
+// - Poll (retrieve new I/O events; execute I/O related callbacks; can also return to the timers phase)
+// - Check (setImmediate)
+// - Close Callbacks (execute all close event callbacks)
+// - Exit or re-run
---
+++ routes.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+
+function requestHandler(req, res) {
+  const { url, method } = req;
+
+  // Home page
+  if (url === "/") {
+    //   Sending HTML Code
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    // The first time this function is encountered, it will register an event listener
+    // The data is streamed in in chunks
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    // Merge all the chunk together
+    // The first time this function is encountered, it will register an event listener
+    // Node won't block and pause. It is registering an event listener (callback function)
+    // This function will get executed on a future time. Node is registering this function and then it will continue to execute the code
+    // We should return here because we don't want to execute the rest of the code below
+    return req.on("end", () => {
+      // We know it's text so we can use toString
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+
+      // This will block the code execution until the file is written
+      // fs.writeFileSync("message.txt", message);
+
+      // This will not block the code execution
+      fs.writeFile("message.txt", message, (err) => {
+        // Redirecting the user
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+}
+
+module.exports = requestHandler;
